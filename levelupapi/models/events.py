1a93509a705cb7c493236392c7771095bb7e9e6e
Shawn Conerton <conerton52@gmail.com>
2021-02-05 11:19:02 -0600

    added models to levelupapi

---
+++ levelupapi/models/events.py
@@ -0,0 +1,13 @@
+
+from django.db import models
+# from django.contrib.auth.models import Events
+from django.contrib.auth.models import Gamer
+from django.contrib.auth.models import Game
+
+
+class Event(models.Model):
+
+    event_time = models.DateField.auto_now()
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    location = models.CharField(max_length=50)
+    scheduler = models.ForeignKey(Gamer, on_delete=models.CASCADE)
